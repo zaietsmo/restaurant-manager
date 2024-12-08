d79328d4f8e1a37b3c567ff6ce5b039ae7816cd1
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-12-09 01:55:25 +0200

    fix: added render_external_hostname

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -7,7 +7,13 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", 'localhost']
+ALLOWED_HOSTS = []
+
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
