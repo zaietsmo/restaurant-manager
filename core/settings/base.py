f87945014e07d5ebe35f4b42b1f80329f7ea8a08
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-12-09 01:48:03 +0200

    fix: added build.sh

--- core/settings/base.py
+++ core/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z1)gr1pdkz2dg5!@=z2xcywhi2pw(8h0f)^_uw8)@819k&jhf$"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
