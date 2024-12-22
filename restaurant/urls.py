38172f580c9cf31f9b944da90644584d5f3747f7
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-12-22 19:12:42 +0200

    fix: changed urls (edit -> update)

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,19 +8,21 @@ urlpatterns = [
     path("", views.DishListView.as_view(), name="dish_list"),
     path("dishes/", views.DishListView.as_view(), name="dishes"),
     path("dish/create/", views.DishCreateView.as_view(), name="dish_create"),
-    path("dish/<int:pk>/edit/", views.DishUpdateView.as_view(), name="dish_edit"),
-    path("dish/<int:pk>/delete/", views.DishDeleteView.as_view(), name="dish_delete"),
+    path("dishes/<int:pk>/update/", views.DishUpdateView.as_view(), name="dish_update"),
+    path("dishes/<int:pk>/delete/", views.DishDeleteView.as_view(), name="dish_delete"),
     path("dish-types/", views.DishTypeListView.as_view(), name="dish_type_list"),
     path(
-        "dish-type/create/", views.DishTypeCreateView.as_view(), name="dish_type_create"
+        "dish-types/create/",
+        views.DishTypeCreateView.as_view(),
+        name="dish_type_create",
     ),
     path(
-        "dish-type/<int:pk>/edit/",
+        "dish-types/<int:pk>/update/",
         views.DishTypeUpdateView.as_view(),
-        name="dish_type_edit",
+        name="dish_type_update",
     ),
     path(
-        "dish-type/<int:pk>/delete/",
+        "dish-types/<int:pk>/delete/",
         views.DishTypeDeleteView.as_view(),
         name="dish_type_delete",
     ),
